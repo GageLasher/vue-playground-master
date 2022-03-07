50b6e637f68c795535f95967f3d664b581558f19
Gage Lasher <rsgage21@gmail.com>
2022-03-07 14:33:06 -0700

    commit

---
+++ src/components/ComputedPropertiesExercise.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="computed-properties-exercise container">
+    <div class="row">
+      <div class="col-12">
+        <div class="border p-1">
+          <div class="reverse-message">
+            <p>Original Message: <input type="text" v-model="state.message"></p>
+            <p>Computed Reversed Message: {{state.reversedMessage}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import { computed, reactive } from "vue";
+export default {
+  name: "computed-properties-exercise",
+  setup() {
+    const state = reactive({
+      // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
+      message: "Hello World!",
+      reversedMessage: computed(() => {
+        // all computed properties must return a value, what do you want this one to return?
+        
+        let reversedMessage = state.message.split("").reverse().join("")
+        return reversedMessage
+        
+      }),
+    });
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
